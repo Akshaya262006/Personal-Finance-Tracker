89bd3a19561595a6fe21231ff3f6a0b30003117e
Akshaya Nelaturi <nelaturiakki@gmail.com>
2026-06-18 19:56:18 +0530

    Added dark mode thrme feature

--- js/theme.js
+++ js/theme.js
@@ -2,15 +2,15 @@ const themeBtn =
 document.getElementById("themeBtn");
 if(localStorage.getItem("theme") === "dark"){
     document.body.classList.add("dark-mode");
-    themeBtn.innerText = "☀️ Light Mode";
+    themeBtn.innerText = "Light Mode";
 }
 themeBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark-mode");
     if(document.body.classList.contains("dark-mode")){
         localStorage.setItem("theme","dark");
-        themeBtn.innerText = "☀️ Light Mode";
+        themeBtn.innerText = "Light Mode";
     }else{
         localStorage.setItem("theme","light");
-        themeBtn.innerText = "🌙 Dark Mode";
+        themeBtn.innerText = "Dark Mode";
     }
 });
